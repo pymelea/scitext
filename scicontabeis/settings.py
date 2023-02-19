8355b1d0372ba39d641d5438eaa2e743499802c2
Lily Perera <lilycapetillo86@gmail.com>
2023-02-19 16:42:31 -0300

    Configurations to settings file for access a database in imagen docker of postgres.

--- scicontabeis/settings.py
+++ scicontabeis/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'scicontabeis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# Configurations for postgresql connection
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbsci',
+        'USER': 'lily',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
